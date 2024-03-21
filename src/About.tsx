21cdbb8a4d0d4e8349be5fb4475adb51f403cf79
Andy Young <andy@mutinyhq.com>
2024-03-21 10:31:04 -0400

    feat: again, staging client

--- src/About.tsx
+++ src/About.tsx
@@ -3,9 +3,17 @@ import Mithril, { Component } from "mithril";
 const About: Component = {
   view() {
     return (
-      <div class="page-content">
+      <article class="page-content">
         <h1>About</h1>
-      </div>
+        <div class="row fill-layout flex">
+          <div>
+            <img src="https://placekitten.com/200/200" alt="here, kitty kitty" />
+          </div>
+          <div>
+            <img src="https://placekitten.com/200/200" alt="here, kitty kitty" />
+          </div>
+        </div>
+      </article>
     );
   },
 };
